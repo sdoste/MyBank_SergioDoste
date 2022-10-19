ebc99593103ced54d84f0ab0df0ac824f9372112
sergio <sergiodoste@gmail.com>
2022-10-19 19:35:00 +0200

    couple of tests

--- src/test/java/com/midterm/MyBank/model/accounts/CreditCardTest.java
+++ src/test/java/com/midterm/MyBank/model/accounts/CreditCardTest.java
@@ -14,48 +14,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreditCardTest {
 
     @Test
-    void setCreditLimit() {
+    void TestSetCreditLimit() {
         Address address = new Address("Spain", "Barcelona", "Calle Mallorca", "120");
         LocalDate date1992 = LocalDate.of(1992, 1, 8);
         AccountHolder userFran = new AccountHolder("fran", encryptPassword("fran123"), "Francisco Tilleo", date1992, address);
-        BigDecimal creditLimit = new BigDecimal("0.5");
         BigDecimal interestRate = new BigDecimal("1111");
 
-        //if null interestRate and credit limit
-        CreditCard userFranAccount = new CreditCard(new Money(new BigDecimal("1111")), userFran,null, null);
+        //If credit limit is null
+        CreditCard userFranAccount = new CreditCard(new Money(new BigDecimal("1111")), userFran,null, interestRate);
         assertEquals(new BigDecimal("100.00"), userFranAccount.getCreditLimit().getAmount());
-        assertEquals(new BigDecimal("0.2000"), userFranAccount.getInterestRate());
 
-        //if interestRate lower than 0.1, should be set to 0.1
-        CreditCard userFranAccount2 = new CreditCard(new Money(new BigDecimal("1111")), userFran,null, new BigDecimal("0.002"));
-        assertEquals(new BigDecimal("0.1000"), userFranAccount2.getInterestRate());
-
-        //if creditLimit higher than 100 000, should be set toi 100 000
-        CreditCard userFranAccount3 = new CreditCard(new Money(new BigDecimal("1111")), userFran,new BigDecimal("10005000"), null);
+        //If creditLimit higher than 100 000, should be set toi 100 000
+        CreditCard userFranAccount3 = new CreditCard(new Money(new BigDecimal("1111")), userFran,new BigDecimal("10005000"), interestRate);
         assertEquals(new BigDecimal("100000.00"), userFranAccount3.getCreditLimit().getAmount());
     }
 
     @Test
-    void setInterestRate() {
+    void testSetInterestRate() {
+        Address address = new Address("Spain", "Barcelona", "Calle Mallorca", "120");
+        LocalDate date1992 = LocalDate.of(1992, 1, 8);
+        AccountHolder userFran = new AccountHolder("fran", encryptPassword("fran123"), "Francisco Tilleo", date1992, address);
+        BigDecimal creditLimit = new BigDecimal("0.5");
+
+
+        //if null interestRate
+        CreditCard userFranAccount = new CreditCard(new Money(new BigDecimal("1111")), userFran,creditLimit, null);
+        assertEquals(new BigDecimal("0.2000"), userFranAccount.getInterestRate());
+
+        //if interestRate lower than 0.1, should be set to 0.1
+        CreditCard userFranAccount2 = new CreditCard(new Money(new BigDecimal("1111")), userFran,creditLimit, new BigDecimal("0.002"));
+        assertEquals(new BigDecimal("0.1000"), userFranAccount2.getInterestRate());
+
     }
 
     @Test
-    void getBalance() {
+    void getBalanceAndApplyPenaltyFee() {
     }
 
-    @Test
-    void setBalance() {
-    }
 
-    @Test
-    void testSetCreditLimit() {
-    }
-
-    @Test
-    void getPenaltyFee() {
-    }
-
-    @Test
-    void setPenaltyFee() {
-    }
 }
